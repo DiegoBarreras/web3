ffbf150d98d6b56bc257bf4133bac09eac9b7424
DiegoBarreras <diegobarreras01@gmail.com>
2026-01-04 16:59:18 -0700

    v2

--- blockchainJS/ejBlockchain.js
+++ blockchainJS/ejBlockchain.js
@@ -1,20 +1,37 @@
 const crypto = require('crypto');
+const { diff } = require('util');
+const { measureMemory } = require('vm');
 
 class Block {
-    constructor(index, data, prevHash, hash) {
+    constructor(index, data, prevHash) {
         this.index = index;
         this.timestamp = Date.now();
         this.data = data;
         this.prevHash = prevHash;
+        this.nonce = 0;
         this.hash = this.hashCalc();
     }
 
     hashCalc() {
-        const nonHashedString = this.index + this.timestamp + JSON.stringify(this.data) + this.prevHash;
+        const nonHashedString = this.index + this.timestamp + JSON.stringify(this.data) + this.prevHash + this.nonce;
         return crypto
             .createHash('sha256')
             .update(nonHashedString)
             .digest('hex');
+    }
+
+    mineBlock(difficulty) {
+        const target = '0'.repeat(difficulty);
+        console.time('Measured Time');
+
+        while (this.hash.substring(0, difficulty) !== target) {
+            this.nonce++;
+            this.hash = this.hashCalc();
+        }
+
+        console.log(`Block succesfully mined! Hash: ${this.hash} Nonce: ${this.nonce}`);
+
+        console.timeEnd('Measured Time');
     }
 }
 
@@ -33,7 +50,11 @@ class Blockchain {
     
     addBlock(data) {
         const newBlock = new Block(this.chain.length,data,this.lastBlock().hash);
-        return this.chain.push(newBlock);
+
+        console.log('Mining a new block...')
+        newBlock.mineBlock(5);
+
+        this.chain.push(newBlock);
     }
 }
 
